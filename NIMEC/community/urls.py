65023aea2192f3b514f4b4821a64f3ec8916c172
Phạm Quốc Huy <quochuyks2k3@gmail.com>
2024-09-23 15:26:45 +0700

    update

--- NIMEC/community/urls.py
+++ NIMEC/community/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('register', views.register, name = 'register'),
     path('homepage', views.homepage, name = 'homepage'),
     path('functions_duties', views.functions_duties, name = 'functions_duties'),
     path('institute_introduction', views.institute_introduction, name = 'institute_introduction'),
     path('institute_leadership', views.institute_leadership, name = 'institute_leadership'),
     path('organizational_structure', views.organizational_structure, name = 'organizational_structure'),
+    path('staff_organization', views.staff_organization, name = 'staff_organization'),
+    path('administrative', views.administrative, name = 'administrative'),
+    path('finance_accounting', views.finance_accounting, name = 'finance_accountinge'),
+    path('scientific_management', views.scientific_management, name = 'scientific_management'),
+    path('tranning', views.tranning, name = 'tranning'),
 ]
--- NIMEC/community/views.py
+++ NIMEC/community/views.py
@@ -3,17 +3,35 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'community/home.html')
 
+def register(request):
+    return render(request, 'community/register.html')
+
 def homepage(request):
     return render(request, 'community/homepage.html')
 
 def functions_duties(request):
-    return render(request, 'community/functions_duties.html')
+    return render(request, 'community/introduction/functions_duties.html')
 
 def institute_introduction(request):
-    return render(request, 'community/institute_introduction.html')
+    return render(request, 'community/introduction/institute_introduction.html')
 
 def institute_leadership(request):
-    return render(request, 'community/institute_leadership.html')
+    return render(request, 'community/introduction/institute_leadership.html')
 
 def organizational_structure(request):
-    return render(request, 'community/organizational_structure.html')
+    return render(request, 'community/introduction/organizational_structure.html')
+
+def staff_organization(request):
+    return render(request, 'community/introduction/structure/staff_organization.html')
+
+def administrative(request):
+    return render(request, 'community/introduction/structure/administrative.html')
+
+def finance_accounting(request):
+    return render(request, 'community/introduction/structure/finance_accounting.html')
+
+def scientific_management(request):
+    return render(request, 'community/introduction/structure/scientific_management.html')
+
+def tranning(request):
+    return render(request, 'community/tranning.html')
